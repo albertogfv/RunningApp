4998ed6e8a6009ee499b05b4bd7cc23fc57c724f
albertogfv <albertogfv@gmail.com>
2020-08-20 14:44:31 -0700

    added db, di, other packages

---
+++ app/src/main/java/com/albertogfv/runningapp/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.albertogfv.runningapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+}
---
+++ app/src/main/java/com/albertogfv/runningapp/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.albertogfv.runningapp.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_databse.db"
+}
