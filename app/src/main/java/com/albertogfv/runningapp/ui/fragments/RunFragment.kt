1f2e43e72350a9f02c0ec0c2cfe62146092ef567
albertogfv <albertogfv@gmail.com>
2020-08-22 14:12:33 -0700

    updated fragments and main activity layout

--- app/src/main/java/com/albertogfv/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/albertogfv/runningapp/ui/fragments/RunFragment.kt
@@ -1,15 +1,25 @@
 package com.albertogfv.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.albertogfv.runningapp.R
 import com.albertogfv.runningapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        fab.setOnClickListener{
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
