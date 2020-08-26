cbcd1d302500a237dc4cf3d3a2398cb3978cc1e4
albertogfv <albertogfv@gmail.com>
2020-08-26 09:56:11 -0700

    add location updates

--- app/src/main/java/com/albertogfv/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/albertogfv/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,13 @@
 package com.albertogfv.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.albertogfv.runningapp.R
+import com.albertogfv.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.albertogfv.runningapp.services.TrackingService
 import com.albertogfv.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,12 +24,20 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         mapView.onCreate(savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
 
         mapView.getMapAsync{
             map = it
         }
     }
 
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also{
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
