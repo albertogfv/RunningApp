1bab2e5b5c518e8eb2dc8ec66de0a209a4703e2c
albertogfv <albertogfv@gmail.com>
2020-09-01 23:03:47 -0700

    add name and weight

--- app/src/main/java/com/albertogfv/runningapp/di/AppModule.kt
+++ app/src/main/java/com/albertogfv/runningapp/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.albertogfv.runningapp.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.albertogfv.runningapp.db.RunningDatabase
+import com.albertogfv.runningapp.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.albertogfv.runningapp.other.Constants.KEY_NAME
+import com.albertogfv.runningapp.other.Constants.KEY_WEIGHT
 import com.albertogfv.runningapp.other.Constants.RUNNING_DATABASE_NAME
+import com.albertogfv.runningapp.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,4 +35,24 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun providesName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Provides
+    @Singleton
+    fun providesWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Provides
+    @Singleton
+    fun providesFirstTimeToggle(sharedPref: SharedPreferences) = sharedPref.getBoolean(
+        KEY_FIRST_TIME_TOGGLE, true)
+
+
 }
